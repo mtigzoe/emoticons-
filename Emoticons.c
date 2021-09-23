265ad17fe76239d76b086ea7c3a0f36316861cbb
mtigzoe <mloznea@g.clemson.edu>
2021-09-23 18:37:00 -0400

    emoticons.c

---
+++ Emoticons.c
@@ -0,0 +1,80 @@
+//Emoticons 
+
+#include <stdio.h>
+
+int main(void){
+
+//1.) rabbit emoticon 
+
+char rabbit[5] = "()()";
+char rabbit2[8] = "=(-,-)=";
+char rabbit3[8] = "(\")_(\")";
+
+printf("Rabbit emoticon is \n  %s\n%s\n%s\n", rabbit, rabbit2, rabbit3);
+
+//2.)cat emoticon
+
+char cat[18] = "(>^.^<) Meow....."; 
+printf("\n\nCat emoticon: %s\n\n", cat);
+
+//3.)dog emoticon
+
+char dog[8] ="/(^.^)\\";
+char dog2[4] = ":o\?";
+ 
+printf("\n\nDog emoticon: %s or %s\n\n", dog, dog2); 
+
+//4.)bird emoticon
+
+char bird[6] = "(*v*)"; 
+
+printf("\n\nbird emoticon\n %s\n\n", bird); 
+
+
+//5.)fish emoticon
+
+
+char fish[8] = "<\")))><";
+char fish2[8] = "<(\'O\')>";
+
+printf("\n\nFish emoticon: %s or %s\n\n", fish, fish2);
+
+
+//6.) shark emoticon 
+
+
+char shark[7] = "<(((\">";
+
+printf("\n\nShark emoticon: %s\n\n", shark);
+
+//7.) crab emoticon
+
+char crab[12] = "(\\/)!_!(\\/)";
+
+printf("\n\nCrab emoticon: %s\n\n", crab); 
+
+//8.) rat emoticon
+
+char rat[11] = "<:3(   )~~";
+
+printf("\n\nrat emoticon: %s\n\n", rat);
+
+//9.) spider emoticon
+
+char spider[9] = "/\\(00)/\\"; 
+
+printf("\n\nspider emoticon: %s\n\n", spider);
+
+//10. snail emoticon)
+
+char bee[4] =  "( )";
+char bee2[8] = ":(III)-";
+char bee3[4] = "( )";
+
+
+printf("\n\nbee emoticon:\n  %s\n%s\n  %s\n\n", bee, bee2, bee3);
+
+
+return 0;
+}
+
